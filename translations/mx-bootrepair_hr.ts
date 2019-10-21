c50df8aa87fbb8b9066cc2688270b1033710547f
Adrian <adrian@mxlinux.org>
2019-10-21 16:15:40 -0400

    new build

--- translations/mx-bootrepair_hr.ts
+++ translations/mx-bootrepair_hr.ts
@@ -163,7 +163,7 @@
         <location filename="../mainwindow.cpp" line="419"/>
         <location filename="../mainwindow.cpp" line="527"/>
         <location filename="../mainwindow.cpp" line="534"/>
-        <location filename="../mainwindow.cpp" line="594"/>
+        <location filename="../mainwindow.cpp" line="597"/>
         <source>Error</source>
         <translation type="unfinished">Greška</translation>
     </message>
@@ -323,7 +323,7 @@ Jeste li sigurni?</translation>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="595"/>
+        <location filename="../mainwindow.cpp" line="598"/>
         <source>Sorry, could not mount %1 partition</source>
         <translation type="unfinished"></translation>
     </message>
